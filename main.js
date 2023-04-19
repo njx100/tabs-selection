d6cb365c054dfd95e329f63a2b218ff60adfaaa0
Dnnguyen <dnnguyen100@gmail.com>
2023-04-19 09:54:30 +0700

    complete basic functions

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const btnEl = document.querySelectorAll(".btn");
+const tabsEl = document.querySelector(".tabs");
+const contentEl = document.querySelectorAll(".content");
 
+
+tabsEl.addEventListener("click", (event) => {
+    const id = event.target.dataset.id;
+
+    if (id) {
+        btnEl.forEach((btn) => {
+            btn.classList.remove("active");
+        })
+        event.target.classList.add("active");
+        contentEl.forEach((content) => {
+            content.classList.remove("live");
+        })
+        const selectedContent = document.getElementById(id);
+        selectedContent.classList.add("live");
+    }
+
+
+    
+})
